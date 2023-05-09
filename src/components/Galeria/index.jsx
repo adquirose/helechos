92d570f85eaecfc7e0e8b1505bd530890e6a0091
Arturo Quiros <adquirose@gmail.com>
2023-05-09 19:42:24 -0400

    ruta imagenes

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -10,123 +10,123 @@ const MyGallery = () => (
   <Gallery>
     <div className='gallery-container'>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/1.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/1.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/1.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/1.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/1.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/1.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/2.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/2.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/2.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/2.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/2.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/2.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/3.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/3.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/3.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/3.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/3.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/3.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/4.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/4.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/4.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/4.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/4.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/4.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/5.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/5.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/5.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/5.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/5.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/5.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/6.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/6.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/6.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/6.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/6.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/6.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/8.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/8.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/8.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/8.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/8.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/8.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/9.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/9.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/9.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/9.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/9.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/9.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/10.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/10.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/10.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/10.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/10.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/10.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/11.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/11.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/11.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/11.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/11.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/11.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/12.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/12.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/12.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/12.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/12.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/12.jpg" />
       )}
     </Item>
     <Item
-      original="https://www.lanube360.com/temporales-helechos/images/galeria/13.jpg"
-      thumbnail="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/13.jpg"
+      original="https://www.lanube360.com/temp-helechos/images/galeria/13.jpg"
+      thumbnail="https://www.lanube360.com/temp-helechos/images/galeria/thumb/13.jpg"
       width="1024"
       height="576"
     >
       {({ ref, open }) => (
-        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temporales-helechos/images/galeria/thumb/1.jpg" />
+        <img className="image-gallery" ref={ref} onClick={open} src="https://www.lanube360.com/temp-helechos/images/galeria/thumb/1.jpg" />
       )}
     </Item>
     </div>
